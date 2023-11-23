17532ae5a0461e1ecf11402d73a4975c400ed375
Alexei Rusu <rusualexei1995@gmail.com>
2023-11-23 11:45:30 -0500

    starting components

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -1,8 +1,13 @@
-import React from 'react'
+import { TextInput, View } from "react-native"
+import PrimaryButton from "../components/PrimaryButton"
 
 function StartGameScreen() {
   return (
-    <div>StartGameScreen</div>
+    <View>
+        <TextInput />
+        <PrimaryButton>Reset</PrimaryButton>
+        <PrimaryButton>Confirm</PrimaryButton>
+    </View>
   )
 }
 
