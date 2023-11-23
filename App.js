90bf105316982d3dd226ed54942626350f5d8224
Alexei Rusu <rusualexei1995@gmail.com>
2023-11-23 14:01:24 -0500

    styled start game screen

--- App.js
+++ App.js
@@ -1,11 +1,18 @@
 import { LinearGradient } from 'expo-linear-gradient'
-import { StyleSheet } from 'react-native';
+import { StyleSheet, ImageBackground } from 'react-native';
 import StartGameScreen from './screens/StartGameScreen';
 
 export default function App() {
   return (
     <LinearGradient colors={['#ddb52f', '#4e0329']} style={styles.rootScreen}>
-       <StartGameScreen />
+      <ImageBackground 
+        source={require('./assets/images/background.png')} 
+        resizeMode='cover' 
+        style={styles.rootScreen} 
+        imageStyle={styles.backgroundImage}
+        >
+        <StartGameScreen />
+      </ImageBackground>
     </LinearGradient>
    
   );
@@ -15,5 +22,8 @@ const styles = StyleSheet.create({
   rootScreen: {
     flex: 1,
     // backgroundColor: '#ddb52f',
+  },
+  backgroundImage:{
+    opacity: 0.15,
   }
 });
