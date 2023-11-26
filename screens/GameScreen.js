5fe025412e86fe06f52fa501651b19b7e671ba6a
Alexei Rusu <rusualexei1995@gmail.com>
2023-11-26 10:15:54 -0500

    Title component

--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -1,9 +1,30 @@
-import { Text } from 'react-native'
+import { Text, View, StyleSheet } from 'react-native'
+import Title from '../components/Title'
 
-function GameScreen() {
+function GameScreen({}) {
   return (
-    <Text>Game Over!</Text>
+    <View style={styles.screen}>
+      <Title>Opponent's Guess</Title>
+      {/* Guess */}
+      <View>
+        <Text>Higher or lower?</Text>
+        {/* + */}
+        {/* - */}
+      </View>
+      <View>
+        {/* LOG ROUNDS */}
+      </View>
+    </View>
   )
 }
 
 export default GameScreen
+
+
+const styles = StyleSheet.create({
+  screen: {
+    flex: 1,
+    padding: 24,
+  },
+
+})
