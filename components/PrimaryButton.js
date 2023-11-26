7de8ff306831682000f26287cf79d39f7e95ce87
Alexei Rusu <rusualexei1995@gmail.com>
2023-11-26 09:41:21 -0500

    main screen buttons fuc

--- components/PrimaryButton.js
+++ components/PrimaryButton.js
@@ -1,18 +1,15 @@
 import { View, Text, Pressable, StyleSheet} from "react-native"
 
-function PrimaryButton(props) {
+function PrimaryButton({ children, onPress }) {
 
-  function pressHandler(){
-    console.log('pressed!')
-  }
   return (
     <View style={styles.buttonOuterContainer}>
       <Pressable
         style={({pressed}) => pressed ?[ styles.buttonInnerContainer, styles.pressed] : styles.buttonInnerContainer}
-        onPress={pressHandler}
+        onPress={onPress}
         android_ripple={{ color: "#640233" }}
       >
-        <Text style={styles.buttonText}>{props.children}</Text>
+        <Text style={styles.buttonText}>{children}</Text>
       </Pressable>
     </View>
   );
