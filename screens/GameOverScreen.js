0c54f7a0cefc51c0954683fa6672c4da2f068bed
Alexei Rusu <rusualexei1995@gmail.com>
2023-12-03 13:57:55 -0500

    useWindowDimensions hook for trans from landscape

--- screens/GameOverScreen.js
+++ screens/GameOverScreen.js
@@ -1,4 +1,4 @@
-import { Image, Text, View, StyleSheet } from "react-native"
+import { Image, Text, View, StyleSheet, Dimensions } from "react-native"
 
 import Title from '../components/ui/Title'
 import PrimaryButton from '../components/ui/PrimaryButton'
@@ -20,6 +20,8 @@ function GameOverScreen({roundsNumber, userNumber, onStartNewGame }) {
 
 export default GameOverScreen
 
+const deviceWidth = Dimensions.get("window").width
+
 const styles = StyleSheet.create({
   rootContainer: {
     flex: 1,
@@ -34,9 +36,9 @@ const styles = StyleSheet.create({
     color: 'white',
   },
   imageContainer: {
-    width: 300,
-    height: 300,
-    borderRadius: 150,
+    width: deviceWidth < 380 ? 150 : 300,
+    height: deviceWidth < 380 ? 150 : 300,
+    borderRadius: deviceWidth < 380 ? 75 : 150,
     borderWidth: 1,
     borderColor: 'white',
     overflow: 'hidden',
